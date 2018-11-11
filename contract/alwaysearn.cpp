6ae7ed6a6512fea460757caebc81c8de2ca6fff9
Bryan <psy2848048@gmail.com>
2018-11-10 23:09:42 -0800

    Initial contract code

---
+++ contract/alwaysearn.cpp
@@ -0,0 +1,33 @@
+#include "alwaysearn.hpp"
+
+namespace langchain {
+
+void alwaysearn::addbid(
+        eosio::name   name,
+        std::string   strname,
+        std::string   website,
+        uint64_t      price)
+{
+
+    bidder bidderstable( _self, _self);
+    bidderstable.emplace( _self, [&]( auto& s ) { 
+       s.id         = bidderstable.available_primary_key();
+       s.name       = name;
+       s.strname    = strname;
+       s.website    = website;
+       s.price      = price;
+    }); 
+}
+
+void alwaysearn::deleteall(){
+    require_auth(_self);
+    bidder bidderstable(_self, _self);
+
+    auto itr = bidderstable.begin();
+    while (itr != bidderstable.end() ){
+       bidderstable.erase( itr );
+       itr = bidderstable.begin();
+    }
+}
+
+EOSIO_DISPATCH( langchain::alwaysearn, (addbid)(deleall) )
---
+++ contract/alwaysearn.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <eosiolib/asset.hpp>
+#include <eosiolib/eosio.hpp>
+
+#include <string>
+
+namespace langchain {
+
+   class [[eosio::contract("alwaysearn")]] alwaysearn : public eosio::contract {
+      private:
+         struct [[eosio::table]] bidders{
+             uint64_t       id;
+             uint64_t       name;
+             std::string    strname;
+             std::string    website;
+             uint64_t       price;
+
+             uint64_t primary_key()const { return id; }
+         };
+
+         struct [[eosio::table]] minprices{
+             uint64_t       price;
+
+             uint64_t primary_key()const { return price; }
+         };
+
+      public:
+         using contract::contract;
+
+         [[eosio::action]]
+         void addbid( eosio::name  name,
+                      std::string  strname,
+                      std::string  website,
+                      uint64_t     price);
+
+         [[eosio::action]]
+         void deleteall();
+
+         typedef eosio::multi_index< "bidder"_n, bidders > bidder;
+         typedef eosio::multi_index< "minprice"_n, minprices > minprice;
+
+   };
+}
